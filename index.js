4323fcd58f18c499a627375e69bd6a31ddd4b948
reymundadwi <reymunda613@gmail.com>
2021-04-27 17:55:33 +0700

    Get request

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+const programmingBooks = [
+    {title: "Algorithm & Data Structure",description:"Basic algorithm concept",price:100000,id:1},
+    {title: "Front End Lesson",description:"Basic front end with HTML,CSS,JS,and ReactJS",price:25000,id:2},
+    {title: "Back End Lesson",description:"Basic back end with ExpressJS",price:20000,id:3},
+    {title: "Fullstack Web Developer",description:"With MongoDB,ExpressJS,ReactJS,NodeJS",price:100000,id:4}
+]
+
+app.get('/',(req,resp) => {
+    resp.send('This is my first API with ExpressJS') 
+})
+
+app.get('/api/programmingbooks/',(req,resp) => {
+    resp.send(programmingBooks)
+})
+app.listen(8080)
